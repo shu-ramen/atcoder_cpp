f0c3374530c0d22121f75a5177fc0682f0bbb642
Shu Isaka - Home <shu.h7.research@gmail.com>
2019-06-10 00:38:20 +0900

    Created template.cpp

---
+++ template.cpp
@@ -0,0 +1,58 @@
+//=template==================================================================//
+// include
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <sstream>
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <string>
+
+// namespace
+using namespace std;
+
+// define
+#define FOR(i,a,b) for(int (i)=(a); (i)<(b); (i)++)
+#define REP(i,n) FOR(i,0,n)
+#define MIN_V(v) *min_element(v.begin(), v.end())
+#define MAX_V(v) *max_element(v.begin(), v.end())
+
+// typedef
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<long> VL;
+typedef vector<VL> VVL;
+typedef vector<string> VS;
+typedef long long LL;
+typedef unsigned long long ULL;
+
+// conversion
+inline int toInt(string s) {int v; istringstream sin(s); sin>>v; return v;}
+inline int countChar(string s, char c) {int cnt,i; cnt=0; REP (i,s.size()) {if (s[i]==c) cnt++;} return cnt;}
+template<class T> inline string toString(T x) {ostringstream sout; sout<<x; return sout.str();}
+//===========================================================================//
+
+//=function prototype========================================================//
+
+//===========================================================================//
+
+//=variables=================================================================//
+
+//===========================================================================//
+int main(void)
+{
+  // Scan begin.
+  
+  // Scan done.
+
+  // Code begin.
+
+  // Code done.
+
+  return 0;
+}
+
+//=functions=================================================================//
+
+//===========================================================================//
